4d63089d9360c4888c51ad12ebef33d4980f9f29
theCodeHog <paolatosca@outlook.com>
2020-10-19 14:29:05 +0200

    initial commit

---
+++ src/test/java/com/webservice/pokedex/ResTfulPokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulPokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
