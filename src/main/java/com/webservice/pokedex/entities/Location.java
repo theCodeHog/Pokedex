b985a1f45bf94d77f5fc415439c23c5d8ec2dadb
theCodeHog <paolatosca@outlook.com>
2020-10-30 16:01:02 +0100

    added Location entity

--- src/main/java/com/webservice/pokedex/entities/Location.java
+++ src/main/java/com/webservice/pokedex/entities/Location.java
@@ -1,4 +1,55 @@
 package com.webservice.pokedex.entities;
 
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
 public class Location {
+
+    @Id
+    private String id;
+    private String name;
+    private Object region;
+    private List<Object> areas;
+
+    public Location(){}
+
+    public Location(String id, String name, Object region, List<Object> areas) {
+        this.id = id;
+        this.name = name;
+        this.region = region;
+        this.areas = areas;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getRegion() {
+        return region;
+    }
+
+    public void setRegion(Object region) {
+        this.region = region;
+    }
+
+    public List<Object> getAreas() {
+        return areas;
+    }
+
+    public void setAreas(List<Object> areas) {
+        this.areas = areas;
+    }
 }
