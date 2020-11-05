f59216e51733015e40dd83bd4232951e2f39cb08
theCodeHog <paolatosca@outlook.com>
2020-11-05 13:04:59 +0100

    Added docúmentation to user endpoint

--- src/main/java/com/webservice/pokedex/controllers/UserController.java
+++ src/main/java/com/webservice/pokedex/controllers/UserController.java
@@ -2,6 +2,12 @@ package com.webservice.pokedex.controllers;
 
 import com.webservice.pokedex.entities.User;
 import com.webservice.pokedex.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,42 +23,74 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Get all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all users",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "404", description = "No users found",
+                    content = @Content) })
     @GetMapping
     @Secured("ROLE_USER")
     public ResponseEntity<List<User>> findAllUsers(){
         var users = userService.findAll();
-        return ResponseEntity.ok(users); //Returns statuscode 200
+        return ResponseEntity.ok(users);
     }
 
+    @Operation(summary = "Get a user by their id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the user by id",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content) })
     @GetMapping("/{id}")
     @Secured("ROLE_ADMIN")
-    public ResponseEntity<User> findUserById(@PathVariable String id){
-        return ResponseEntity.ok(userService.findById(id)); //Returns statuscode 200
+    public ResponseEntity<User> findUserById(@Parameter(description = "id of user to be searched") @PathVariable String id){
+        return ResponseEntity.ok(userService.findById(id));
     }
 
-    @GetMapping("/username/{username}") //Don't forget  to write the http address correctly!
+    @Operation(summary = "Get a user by their username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the user by username",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content) })
+    @GetMapping("/username/{username}")
     @Secured("ROLE_USER")
-    public ResponseEntity<User> findUserByUsername(@PathVariable String username){
-        return ResponseEntity.ok(userService.findByUsername(username)); //Returns statuscode 200
+    public ResponseEntity<User> findUserByUsername(@Parameter(description = "username of user to be searched") @PathVariable String username){
+        return ResponseEntity.ok(userService.findByUsername(username));
     }
 
+    @Operation(summary = "Create a user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Created the user succesfully.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "404", description = "User not found", //TODO: test this one
+                    content = @Content) })
     @PostMapping
     @Secured("ROLE_ADMIN")
-    public ResponseEntity<User> saveUser(@Validated @RequestBody User user){//@Validated checks the @NOT_EMPTY and such in User Entity
-        return ResponseEntity.ok(userService.save(user)); //Returns statuscode 200
+    public ResponseEntity<User> saveUser(@Validated @RequestBody User user) {//@Validated checks the @NOT_EMPTY and such in User Entity
+        return ResponseEntity.ok(userService.save(user));
     }
 
+    @Operation(summary = "Update a user")
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Updated the user succesfully. No content to return.")})
     @PutMapping("/{id}")
     @Secured({"ROLE_ADMIN", "ROLE_USER"})
-    @ResponseStatus(HttpStatus.NO_CONTENT) //Returns statuscode 204
-    public void updateUser(@PathVariable String id, @RequestBody User user){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateUser(@Parameter(description = "id of user to be updated") @PathVariable String id, @RequestBody User user){
         userService.update(id, user);
     }
 
+    @Operation(summary = "Delete a user")
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Deleted the user succesfully. No content to return.")})
     @DeleteMapping("/{id}")
     @Secured("ROLE_ADMIN")
-    @ResponseStatus(HttpStatus.NO_CONTENT) //Returns statuscode 204
-    public void deleteUser(@PathVariable String id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@Parameter(description = "id of user to be deleted")@PathVariable String id){
         userService.delete(id);
     }
 
--- src/main/java/com/webservice/pokedex/entities/User.java
+++ src/main/java/com/webservice/pokedex/entities/User.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 import java.util.List;
 
 public class User {
