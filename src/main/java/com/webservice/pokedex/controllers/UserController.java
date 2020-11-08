f209694c7680c98cf41b6d7f9c7dcebc2dcbeb82
theCodeHog <paolatosca@outlook.com>
2020-11-08 21:12:06 +0100

    last small fixes

--- src/main/java/com/webservice/pokedex/controllers/UserController.java
+++ src/main/java/com/webservice/pokedex/controllers/UserController.java
@@ -20,18 +20,19 @@ import java.util.List;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    //USE Basic Auth - Username: admin | Password: admin123 - to access everything as admin
+    //USE Basic Auth - Username: testy | Password: testy101 - to access endpoints as user
+
     @Autowired
     private UserService userService;
 
     @Operation(summary = "Get all users")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found all users",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = User.class)) }),
-            @ApiResponse(responseCode = "404", description = "No users found",
-                    content = @Content) })
+            @ApiResponse(responseCode = "200", description = "Found all users", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden - You are not allowed here!", content = @Content),
+            @ApiResponse(responseCode = "404", description = "No users found", content = @Content) })
     @GetMapping
-    @Secured("ROLE_USER")
+    @Secured("ROLE_USER") // users can access this - not anonymous people
     public ResponseEntity<List<User>> findAllUsers(){
         var users = userService.findAll();
         return ResponseEntity.ok(users);
@@ -39,11 +40,9 @@ public class UserController {
 
     @Operation(summary = "Get a user by their id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found the user by id",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = User.class)) }),
-            @ApiResponse(responseCode = "404", description = "User not found",
-                    content = @Content) })
+            @ApiResponse(responseCode = "200", description = "Found the user by id", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden - You are not allowed here!", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content)})
     @GetMapping("/{id}")
     @Secured("ROLE_ADMIN")
     public ResponseEntity<User> findUserById(@Parameter(description = "id of user to be searched") @PathVariable String id){
@@ -52,11 +51,9 @@ public class UserController {
 
     @Operation(summary = "Get a user by their username")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found the user by username",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = User.class)) }),
-            @ApiResponse(responseCode = "404", description = "User not found",
-                    content = @Content) })
+            @ApiResponse(responseCode = "200", description = "Found the user by username", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden - You are not allowed here!", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content) })
     @GetMapping("/username/{username}")
     @Secured("ROLE_USER")
     public ResponseEntity<User> findUserByUsername(@Parameter(description = "username of user to be searched") @PathVariable String username){
@@ -65,11 +62,9 @@ public class UserController {
 
     @Operation(summary = "Create a user")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Created the user succesfully.",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = User.class)) }),
-            @ApiResponse(responseCode = "418", description = "I am a teapot. (Cannot create a user without a password)",
-                    content = @Content) })
+            @ApiResponse(responseCode = "200", description = "Created the user succesfully.", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden - You are not allowed here!", content = @Content),
+            @ApiResponse(responseCode = "418", description = "I am a teapot. (Cannot create a user without a password)", content = @Content) })
     @PostMapping
     @Secured("ROLE_ADMIN")
     public ResponseEntity<User> saveUser(@Validated @RequestBody User user) {//@Validated checks the @NOT_EMPTY and such in User Entity
@@ -77,8 +72,11 @@ public class UserController {
     }
 
     @Operation(summary = "Update a user")
-    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Updated the user succesfully. No content to return.", content = @Content),
-            @ApiResponse(responseCode = "401", description = "Not authorized", content = @Content)} )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Updated the user succesfully. No content to return.", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Not authorized", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden - You are not allowed here!", content = @Content)
+    })
     @PutMapping("/{id}")
     @Secured({"ROLE_USER", "ROLE_ADMIN"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -87,7 +85,10 @@ public class UserController {
     }
 
     @Operation(summary = "Delete a user")
-    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Deleted the user succesfully. No content to return.")})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deleted the user succesfully. No content to return."),
+            @ApiResponse(responseCode = "403", description = "Forbidden - You are not allowed here!", content = @Content)
+    })
     @DeleteMapping("/{id}")
     @Secured("ROLE_ADMIN")
     @ResponseStatus(HttpStatus.NO_CONTENT)
--- src/main/java/com/webservice/pokedex/services/UserService.java
+++ src/main/java/com/webservice/pokedex/services/UserService.java
@@ -4,10 +4,7 @@ import com.webservice.pokedex.entities.User;
 import com.webservice.pokedex.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,19 +19,20 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
-
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
     public User findById(String id) {
-        return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Could not find the user by id %s.", id)));
+        return userRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        String.format("Could not find the user by id %s.", id)));
     }
 
     public User findByUsername(String username){
         var user = userRepository.findByUsername(username);
-        return user.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Could not find the user by username %s.", username)));
+        return user.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                String.format("Could not find the user by username %s.", username)));
     }
 
     public User save(User user){
@@ -48,7 +46,8 @@ public class UserService {
     public void update(String id, User userUpdate){
 
         var loggedInUser = SecurityContextHolder.getContext().getAuthentication();
-        boolean isAdmin = loggedInUser.getAuthorities().stream().anyMatch(ga -> ga.getAuthority().equals("ROLE_ADMIN"));
+        boolean isAdmin = loggedInUser.getAuthorities().stream().anyMatch(ga ->
+                ga.getAuthority().equals("ROLE_ADMIN"));
 
         User userToUpdate = userRepository.findById(id).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
@@ -66,7 +65,8 @@ public class UserService {
 
     public void delete(String id){
         if(!userRepository.existsById(id)){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Could not find the following id %s.", id));
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("Could not find the following id %s.", id));
         }
         userRepository.deleteById(id);
     }
